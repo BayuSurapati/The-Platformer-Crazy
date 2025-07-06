4fd0cb878d14381e0a0eec5845167e48656e5a31
BayuSurapati <nyomanbayu.surapati@gmail.com>
2025-07-06 16:48:45 +0700

    Jumping and Direction
    Player can now jump and facing to a right direction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,10 +8,14 @@ using UnityEngine.Scripting.APIUpdating;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
+    public float jumpForce, gravityScale;
+    private float yStore;
 
     public CharacterController charCon;
     private CameraController cam;
     private Vector3 moveAmount;
+
+    public float rotateSpeed = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,11 +26,11 @@ public class PlayerController : MonoBehaviour
     {
         if (!charCon.isGrounded)
         {
-            moveAmount.y = moveAmount.y + (Physics.gravity.y * Time.fixedDeltaTime);
+            moveAmount.y = moveAmount.y + (Physics.gravity.y * gravityScale * Time.fixedDeltaTime);
         }
         else
         {
-            moveAmount.y = Physics.gravity.y * Time.deltaTime;
+            moveAmount.y = Physics.gravity.y * gravityScale * Time.deltaTime;
         }
         
     }
@@ -34,14 +38,30 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float yStore = moveAmount.y;
+        yStore = moveAmount.y;
         //transform.position = new Vector3(transform.position.x + (Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime), transform.position.y, transform.position.z + (Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime));
         moveAmount = (cam.transform.forward * Input.GetAxisRaw("Vertical")) + (cam.transform.right * Input.GetAxisRaw("Horizontal"));
         moveAmount.y = 0f;
 
         moveAmount = moveAmount.normalized;
 
+        if (moveAmount.magnitude > 0.1f) 
+        {
+            if (moveAmount != Vector3.zero)
+            {
+                transform.rotation = Quaternion.LookRotation(moveAmount);
+            }
+        }
+
         moveAmount.y = yStore;
+
+        if (charCon.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveAmount.y = jumpForce;
+            }
+        }
 
         charCon.Move(new Vector3(moveAmount.x * moveSpeed, moveAmount.y * moveSpeed, moveAmount.z * moveSpeed) * Time.deltaTime);
 
